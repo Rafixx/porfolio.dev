c9b0d1f04a1e43d65af0955cadb4d646274a012b
Rafa <serra.raf@gmail.com>
2025-03-20 10:43:22 +0100

    Actualizar dependencias de Astro y Flowbite; ajustar configuración de Tailwind CSS

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import flowbite from 'flowbite/plugin';
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-     "./node_modules/flowbite/**/*.js"
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/flowbite/**/*.js'
   ],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [
-    require('flowbite/plugin')
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    flowbite
   ],
 }
